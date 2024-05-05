c47b1a849c6f07c6093d1341e304100a1b0caaae
Edward <edeetee@gmail.com>
2024-05-06 10:33:31 +1200

    homerow mods

--- keyboards/lily58/keymaps/Edward/config.h
+++ keyboards/lily58/keymaps/Edward/config.h
@@ -32,9 +32,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // #define QUICK_TAP_TERM 0
 #define TAPPING_TERM 150
-#define PERMISSIVE_HOLD
+// #define PERMISSIVE_HOLD
 // #define HOLD_ON_OTHER_KEY_PRESS
-
 
 #undef RGBLIGHT_LED_COUNT
 #define RGBLIGHT_EFFECT_BREATHING
