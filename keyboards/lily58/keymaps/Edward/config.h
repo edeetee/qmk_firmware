60a2129c2458146a1384525c35d10dc000883c69
Edward <edeetee@gmail.com>
2024-12-01 11:22:30 +1300

    faster auto shift

--- keyboards/lily58/keymaps/Edward/config.h
+++ keyboards/lily58/keymaps/Edward/config.h
@@ -35,14 +35,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define PERMISSIVE_HOLD
 // #define HOLD_ON_OTHER_KEY_PRESS
 
+#define AUTO_SHIFT_TIMEOUT 150
+
 // #define EXTRAKEY_ENABLE
 
 #define TAPPING_TOGGLE 2
 
 #define ONESHOT_TAP_TOGGLE 2 /* Tapping this number of times holds the key until tapped once again. */
 #define ONESHOT_TIMEOUT 2000 /* Time (in ms) before the one shot key is released */
-
-#define TAPPING_TERM 200
 
 #define LEADER_PER_KEY_TIMING
 #define LEADER_TIMEOUT 250
